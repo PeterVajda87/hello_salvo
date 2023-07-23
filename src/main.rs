18bb5ff80fd94bed2a4c7f13fea53e7277a9818e
Peter Vajda <peter@showdown>
2023-07-23 14:14:02 +0000

    Initial deploy

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use salvo::conn::rustls::{Keycert, RustlsConfig};
+use salvo::prelude::*;
+
+#[handler]
+async fn greet(req: &mut Request) -> String {
+    let name = req
+        .params()
+        .get("name")
+        .cloned()
+        .unwrap_or("World".to_string());
+    format!("Hello, {}!", &name)
+}
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt().init();
+    let cert = include_bytes!("/etc/letsencrypt/live/showdown.monster/cert.pem").to_vec();
+    let key = include_bytes!("/etc/letsencrypt/live/showdown.monster/privkey.pem").to_vec();
+
+    let router = Router::with_path("<name>").get(greet);
+    let config = RustlsConfig::new(Keycert::new().cert(cert.as_slice()).key(key.as_slice()));
+
+    let listener = TcpListener::new(("0.0.0.0", 5800)).rustls(config.clone());
+    let acceptor = QuinnListener::new(config, ("0.0.0.0", 5800))
+        .join(listener)
+        .bind()
+        .await;
+
+    Server::new(acceptor).serve(router).await;
+}
